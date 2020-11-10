20476a1e2360ec4ebecc4f3b72c664878989b58a
Filipe Ferreira <FilipeZCZ2@hotmail.com>
2020-11-10 16:30:42 -0300

    ciclo de vida Shared Preferences

--- app/src/main/java/com/example/chataula2/MainActivity.java
+++ app/src/main/java/com/example/chataula2/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.chataula2;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.logging.LoggingMXBean;
 
@@ -17,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     public final static String KEY_INTENT = "CHAVE MENSAGEM";
     public static final String TAG_KEY_MESSAGE = "MESSAGE_KEY";
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String KEY_MAINACTIVITY = "KEY_MAINACTIVITY";
+
+    private SharedPreferences sharedPreferences;
+    private String valor;
 
 
     @Override
@@ -24,28 +31,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = this.getSharedPreferences("TAG_PREFERENCES", Context.MODE_PRIVATE);
+
         final EditText ed_mensagem = findViewById(R.id.ed_mensagem);
+        TextView textView = findViewById(R.id.tv_recebida);
+        TextView textView2 = findViewById(R.id.tv_recebida2);
         Button button = findViewById(R.id.bt_enviar);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        button.setOnClickListener((View) -> {
+            String texto = ed_mensagem.getText().toString();
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putExtra(TAG_KEY_MESSAGE, texto);
+            if (!ed_mensagem.isEmpty()) {
+                ed_mensagem.setText(ed_mensagem);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(KEY_MAINACTIVITY, ed_mensagem);
+                editor.apply();
+            }
 
-                String texto = ed_mensagem.getText().toString();
 
-                intent.putExtra(KEY_INTENT, texto);
 
 //                startActivity(intent);
-                int requestCode;
-                startActivityForResult(intent, requestCode: 1);
+ //               int requestCode; --> não precisa digitar, é só colocar o valor
+//                startActivityForResult(intent, 1);
+            startActivity(Intent);
 
             }
         });
-
-
-        Log.i(LOG_TAG, msg:"onCreate");
-
+// não precisa digitar msg, é só digitar o texto entre aspas.
+        Log.i(LOG_TAG,"onCreate");
     }
 
     @Override
@@ -54,8 +68,41 @@ public class MainActivity extends AppCompatActivity {
         if(resultCode == 1){
             if(resultCode == Activity.RESULT_OK){
                 String resposta = data.getStringExtra(MainActivity2.KEY_RESPOSTA);
-                tvResposta.setText(resposta);
+                tv_resposta.setText(resposta);
             }
         }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(LOG_TAG,"onStart");
+        valor = sharedPreferences.getString(MainActivity.KEY_MAINACTIVITY,"Não tem nada no banco");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG,"onResume");
+        tvResposta.setText(valor);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(LOG_TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG,"onDestroy");
+    }
 }
+
--- app/src/main/java/com/example/chataula2/MainActivity2.java
+++ app/src/main/java/com/example/chataula2/MainActivity2.java
@@ -3,7 +3,9 @@ package com.example.chataula2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +14,10 @@ import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-
-
     public final static String KEY_INTENT_MAIN2 = "MENSAGEM_MAIN2";
     public static final String KEY_RESPOSTA = "RESPOSTA"
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +28,18 @@ public class MainActivity2 extends AppCompatActivity {
         TextView tvMessage = findViewById(R.id.tv_recebida2);
         Button button = findViewById(R.id.bt_enviar2);
 
-        String mensagemRecebida = getIntent().getStringExtra(MainActivity.TAG_KEY_MESSAGE);
+        sharedPreferences = this.getSharedPreferences("TAG_PREFERENCES", Context.MODE_PRIVATE);
+
+ //       String mensagemRecebida = getIntent().getStringExtra(MainActivity.TAG_KEY_MESSAGE);
+
+        String mensagem = sharedPreferences.getString(MainActivity.KEY_MAINACTIVITY,"Não existe valor para a chave");
         tvMessage.setText(mensagem);
 
         button.setOnClickListener((View) ->  {
             Intent intent = new Intent();
             String value = editText.getText().ToString();
-            intent.putExtra(KEY_RESPOSTA, value);
-            setResult(Activity.RESULT_OK, intent);
+//            intent.putExtra(KEY_RESPOSTA, value);
+//            setResult(Activity.RESULT_OK, intent);
             finish();
         });
 
